cffd6dab496dfe26d123e88fb8e75d8ef34052cb
Ryan885 <ryan.osbaldiston@liston.school.nz>
2021-06-04 13:07:05 +1200

    04 HOW MUCH V2

---
+++ RPS_Base_Component.py
@@ -0,0 +1,19 @@
+import random
+
+# functions go here
+
+# Main routine goes here
+
+# Lists of valid responses
+yes_no_lists = ["yes", "no"]
+rps_list = ["rock", "paper", "scissors", "xxx"]
+
+# Ask user if they have played before
+# If 'no', show instructions
+
+
+# ask user for # of rounds then loop...
+
+
+# ask user if they want to see their game history.
+# if 'yes' show game history
